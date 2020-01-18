3fab4f8871eb596b9abcf752388e47c8e01ca641
batsuuri <kanaobatsuuri@gmail.com>
2020-01-18 17:58:32 +0800

    shoonii zurag oorchlogddog boloh

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 var activePlayer = 1;
+
 var scores = [0, 0];
+
 var roundScores = 0;
-var dice = Math.floor(Math.random()*6) + 1;
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
 
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+var diceNumber = Math.floor(Math.random()*6) + 1;
 
-document.querySelector(".dice").style.display = "none";
+document.getElementById("score-0").textContent = '0';
+document.getElementById("score-1").textContent = '0';
+document.getElementById("current-0").textContent = '0';
+document.getElementById("current-1").textContent = '0';
 
-console.log("Шоо" + dice);
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = 'none';
+document.querySelector(".btn-roll").addEventListener("click", function() {
+
+              var diceNumber = Math.floor(Math.random()*6) + 1;
+.
+              diceDom.style.display = "block";
+              diceDom.src = 'dice-' + diceNumber + '.png';
+});
