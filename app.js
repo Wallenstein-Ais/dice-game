76fade15185aa08326b0160c173fc433e756be43
batsuuri <kanaobatsuuri@gmail.com>
2020-01-19 02:31:17 +0800

    New game tovchiig ashiglaltand oruulsan

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
-var activePlayer = 0;
-
-var scores = [0, 0];
-
-var roundScore = 0;
-
-var diceNumber = Math.floor(Math.random()*6) + 1;
-
-document.getElementById("score-0").textContent = '0';
-document.getElementById("score-1").textContent = '0';
-document.getElementById("current-0").textContent = '0';
-document.getElementById("current-1").textContent = '0';
+var activePlayer;
+var scores;
+var roundScore;
 
 var diceDom = document.querySelector(".dice");
-diceDom.style.display = 'none';
+initGame();
+function initGame() {
+
+              activePlayer = 0;
+              
+              scores = [0, 0];
+              
+              roundScore = 0;
+              
+              document.getElementById("score-0").textContent = '0';
+              document.getElementById("score-1").textContent = '0';
+              document.getElementById("current-0").textContent = '0';
+              document.getElementById("current-1").textContent = '0';
+
+              document.getElementById("name-0").textContent = "Player 1";
+              document.getElementById("name-1").textContent = "Player 2";
+
+              document.querySelector(".player-0-panel").classList.remove("winner");
+              document.querySelector(".player-1-panel").classList.remove("winner");
+
+              document.querySelector(".player-0-panel").classList.remove("active");
+              document.querySelector(".player-1-panel").classList.remove("active");
+
+              document.querySelector(".player-0-panel").classList.add("active");
+
+              diceDom.style.display = 'none';
+              }
 document.querySelector(".btn-roll").addEventListener("click", function() {
 
               var diceNumber = Math.floor(Math.random()*6) + 1;
@@ -47,6 +64,8 @@ function switchToNextPlayer(){
               activePlayer === 0 ? (activePlayer=1) : (activePlayer = 0);
               document.querySelector(".player-0-panel").classList.toggle("active");
               document.querySelector(".player-1-panel").classList.toggle("active");
+
+              diceDom.style.display = "none";
 }
 
-
+document.querySelector(".btn-new").addEventListener("click", initGame);
